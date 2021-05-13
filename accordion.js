2ba97d6a83a045703aafe4dd76f4aba7c47b3865
Valentine <82944117+Valentine-rich@users.noreply.github.com>
2021-05-13 22:59:37 +0300

    new site 2

---
+++ accordion.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.accordion-item_trigger').forEach((item) =>
+	item.addEventListener('click', () => {
+		const parent = item.parentNode;
+
+	if	(parent.classList.contains('accordion-item--active')) {
+		 parent.classList.remove('accordion-item--active');
+	} else {
+		document
+			.querySelectorAll('.accordion-item')
+			.forEach((child) => child.classList.remove('accordion-item--active'))
+		
+		parent.classList.add('accordion-item--active');
+
+		}
+	})
+) 	
